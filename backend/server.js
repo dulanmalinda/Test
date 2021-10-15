905cd4ba10e1abe68f68684eba4cdedf2b34e881
dulanmalinda <dulanmalinda@gmail.com>
2021-10-15 17:22:59 +0530

    dsad

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(mongooseMorgan({
     connectionString: db.url,
 }))
